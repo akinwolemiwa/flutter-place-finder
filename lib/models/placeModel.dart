b468ab8d22f00980dd8a489b4cf9bbd2772f2979
Siyanbola Akin <akin.siyanbola@gmail.com>
2022-02-21 18:39:33 +0100

    change

--- lib/models/placeModel.dart
+++ lib/models/placeModel.dart
@@ -22,13 +22,14 @@ class Place {
     // calling from json
     // parsing nested objects (REST API)
     return Place(
-        name: json['name'],
-        placeId: json['place_id'],
-        latitude: location['lat'],
-        longitude: location['lng'],
-        photoURL: photos.isEmpty
-            ? Image.network(
-                'https://www.pinclipart.com/picdir/middle/545-5450392_transparent-location-clipart-png-location-map-clip-art.png')
-            : photos.first['photo_reference']);
+      name: json['name'],
+      placeId: json['place_id'],
+      latitude: location['lat'],
+      longitude: location['lng'],
+      photoURL: photos.isEmpty
+          ? Image.network(
+              'https://www.pinclipart.com/picdir/middle/545-5450392_transparent-location-clipart-png-location-map-clip-art.png')
+          : photos.first['photo_reference'],
+    );
   }
 }
